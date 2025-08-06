9c8f7dac04167b5b5eafa251c6f7880eacda8e99
Canossi <d.canossi@engys.com>
2025-08-06 19:50:21 -0300

    Add run options to canoSim++
    \-mesh: if the mesh doesn't exist in the base path, create it by reading the mesh input chart.
    \-init: initilize all fields, according to the initFields input chart.
    \-solve: run the simulation.

--- solvers/canoSimxx.cu
+++ solvers/canoSimxx.cu
@@ -60,17 +60,45 @@ void printRunInfo()
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
-int main()
+int main(int argc, char** argv)
 {
+    std::unordered_set<std::string> runOpt = {"-mesh", "-init", "-solve"};
+
+    // Parse run option
+    if (argc != 2 || !runOpt.count(argv[1]))
+    {
+        std::cout << "Invalid usage!\nPlease provide one of the following "
+            << "run options:\n"
+            << "  -mesh\n"
+            << "  -init\n"
+            << "  -solve\n";
+        std::exit(1);
+    }
+
     printHeader();
     printRunInfo();
 
-    std::cout << "Starting simulation\n" << std::endl;
+    std::string runFlag = argv[1];
+    runFlag.erase(0, 1);
+    if (runFlag == "mesh")
+    {
+        std::cout << "Creating mesh for simulation..." << std::endl;
+        const cartMesh mesh;
 
-    std::cout << "Creating mesh for simulation..." << std::endl;
-    const cartMesh mesh;
+        std::cout << "\nEnd." << std::endl;
+    }
+    else if (runFlag == "init")
+    {
+        std::cout << "Initializing fields\n" << std::endl;
 
-    std::cout << "\nEnd." << std::endl;
+        std::cout << "End." << std::endl;
+    }
+    else if (runFlag == "solve")
+    {
+        std::cout << "Starting simulation\n" << std::endl;
+
+        std::cout << "End." << std::endl;
+    }
 
     return 0;
 }
